0929ace74e0fcd0cb5e6c49ed2e3380af89f9f3a
Nikolay <90929111+Faclok@users.noreply.github.com>
2023-05-23 05:08:15 +0300

    create calculate

---
+++ Smart_business/Assets/View/Scenes/Body/Panels/Analitik/CalculatePanel.cs
@@ -0,0 +1,15 @@
+﻿using Assets.View.Body.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.View.Body.Analyst
+{
+    public class CalculatePanel : PanelContent
+    {
+
+        
+    }
+}
